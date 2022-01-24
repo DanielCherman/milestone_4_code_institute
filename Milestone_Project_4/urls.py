455c2794095c5855f97b62ef4bb9a7cf998d2d16
DanielCherman <daniel10050181@gmail.com>
2022-01-24 14:05:12 +0200

    ⚙️ media added
    Date:      Mon Jan 24 14:05:12 2022 +0200

--- Milestone_Project_4/urls.py
+++ Milestone_Project_4/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from . import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
